e0a5fd814600f7fc5890755885c3360a7fd17ff4
isaiascuvula <bersyteinfo@gmail.com>
2024-12-01 02:18:32 +0200

    migration

---
+++ ShopFullStack/Migrations/20241201001353_AddedCartItemsTable.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopFullStack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCartItemsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cart_customers_customer_id",
+                table: "Cart");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Cart_cart_id",
+                table: "CartItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cart",
+                table: "Cart");
+
+            migrationBuilder.RenameTable(
+                name: "Cart",
+                newName: "Carts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cart_customer_id",
+                table: "Carts",
+                newName: "IX_Carts_customer_id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Carts",
+                table: "Carts",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_cart_id",
+                table: "CartItems",
+                column: "cart_id",
+                principalTable: "Carts",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_customers_customer_id",
+                table: "Carts",
+                column: "customer_id",
+                principalTable: "customers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_cart_id",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_customers_customer_id",
+                table: "Carts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Carts",
+                table: "Carts");
+
+            migrationBuilder.RenameTable(
+                name: "Carts",
+                newName: "Cart");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Carts_customer_id",
+                table: "Cart",
+                newName: "IX_Cart_customer_id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cart",
+                table: "Cart",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cart_customers_customer_id",
+                table: "Cart",
+                column: "customer_id",
+                principalTable: "customers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Cart_cart_id",
+                table: "CartItems",
+                column: "cart_id",
+                principalTable: "Cart",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
