cc86d672dc471945ddc9a4fc303f145e9490242a
isaiascuvula <bersyteinfo@gmail.com>
2024-12-01 02:52:56 +0200

    Cart and customer tables updated

--- ShopFullStack/Migrations/20241201005219_CartTablesUpdated.Designer.cs
+++ ShopFullStack/Migrations/20241201005219_CartTablesUpdated.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShopFullStack.Data;
@@ -11,9 +12,11 @@ using ShopFullStack.Data;
 namespace ShopFullStack.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241201005219_CartTablesUpdated")]
+    partial class CartTablesUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
