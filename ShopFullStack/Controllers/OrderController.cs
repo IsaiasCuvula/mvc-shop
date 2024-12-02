8317df68cda4973b551ee0df340f1b2de63b9cad
isaiascuvula <bersyteinfo@gmail.com>
2024-12-02 04:27:41 +0200

    added Checkout

--- ShopFullStack/Controllers/OrderController.cs
+++ ShopFullStack/Controllers/OrderController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ShopFullStack.Models;
 using ShopFullStack.Services;
+using ShopFullStack.Utilities;
 
 namespace ShopFullStack.Controllers;
 
@@ -9,20 +10,99 @@ public class OrderController: Controller
 {
     
     private readonly OrderService _orderService;
+    private readonly CartService _cartService;
     private readonly CustomerService _customerService;
     private readonly UserManager<IdentityUser> _userManager;
     
     public OrderController(
         OrderService orderService, 
         UserManager<IdentityUser> userManager,
-        CustomerService customerService
+        CustomerService customerService,
+        CartService cartService
     )
     {
         _userManager = userManager;
         _orderService = orderService;
         _customerService = customerService;
+        _cartService = cartService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Details(long id)
+    {
+        Console.WriteLine($"orderId: {id}");
+        var customer = await GetCurrentCustomer();
+        if (customer == null)
+        {
+            return RedirectToAction("OrdersPage", "Order");
+        }
+        var response = await _orderService.GetOrderById(id, customer.Id);
+        var order = response.Data;
+        if (order != null)
+        {
+            Console.WriteLine($"OrderItems: {order.OrderItems.Count}");
+        }
+        return View(order ?? new Order());
     }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> UpdateStatus(long id, OrderStatus status)
+    {
+        // var response = await _orderService.GetOrderById(orderId);
+        // if (order == null)
+        // {
+        //     return NotFound();
+        // }
+        //
+        // order.Status = status;
+        // _context.Update(order);
+        // await _context.SaveChangesAsync();
+
+        //return RedirectToAction(nameof(Details), new { id = order.Id });
+        return RedirectToAction(nameof(Details), new { id = 2 });
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Checkout(long cartId)
+    {
+        var customer = await GetCurrentCustomer();
+
+        if (customer == null)
+        {
+            return RedirectToAction("CartPage", "Cart");
+        }
+        
+        var customerId = customer.Id;
+        
+        var response = await _cartService.GetCartById(cartId, customerId);
+        var cart = response.Data;
+        if (cart == null)
+        {
+            return RedirectToAction("CartPage", "Cart");
+        }
+        else
+        {
+            var customerResponse = await _customerService
+                .GetCustomerById(cart.CustomerId);
+            
+            var order = new Order();
+            order.CustomerId = cart.CustomerId;
+            order.CartId = cartId;
+            order.OrderItems = cart.CartItems;
+            order.Total = cart.CartItems.Sum(ci => ci.Total);
+            if (customerResponse.Data != null)
+            {
+                order.ShippingAddress = customerResponse.Data.Address;
+                order.Customer = customerResponse.Data;
+            }
+            await _orderService.CreateOrder(order);
+            //After placing order clear the cart
+            await _cartService.ClearCartById(cartId, customerId);
+            
+            return RedirectToAction("OrdersPage", "Order");
+        }
+    }
     
     [HttpGet]
     public async Task<IActionResult>  OrdersPage()
@@ -40,49 +120,13 @@ public class OrderController: Controller
             return View(new List<Order>());
         }
     }
-    //
-    // [HttpGet("unpaid")]
-    // public async Task<ActionResult<ApiResponse<List<Order>>>> GetAllUnpaidOrders()
-    // {
-    //     var orders = await _orderService.GetAllUnpaidOrders();
-    //     return Ok(orders);
-    // }
-    //
-    // [HttpGet("returned")]
-    // public async Task<ActionResult<ApiResponse<List<Order>>>> GetAllReturnedOrders()
-    // {
-    //     var orders = await _orderService.GetAllReturnedOrders();
-    //     return Ok(orders);
-    // }
-    //
-    // [HttpPut("{id}")]
-    // public async Task<ActionResult<ApiResponse<Order>>> UpdateOrder(long id, OrderDto dto)
-    // {
-    //     return Ok(await _orderService.UpdateOrder(dto, id));
-    // }
-    //
-    // [HttpPost]
-    // public async Task<ActionResult<ApiResponse<Order>>> CreateOrder(UserOrdersDto dto)
-    // {
-    //     return Ok(await _orderService.CreateOrder(dto));
-    // }
-    //
-    // [HttpGet("{id}")]
-    // public async Task<ActionResult<ApiResponse<Order>>> GetOrderById(long id)
-    // {
-    //     return Ok(await _orderService.GetOrderById(id));
-    // }
-    //
-    // [HttpGet("all")]
-    // public async Task<ActionResult<ApiResponse<List<Order>>>> GetAllOrders()
-    // {
-    //     var orders = await _orderService.GetAllOrders();
-    //     return Ok(orders);
-    // }
-    //
-    // [HttpDelete("{id}")]
-    // public async Task<ActionResult<ApiResponse<Order>>> DeleteOrderById(long id)
-    // {
-    //     return Ok(await _orderService.DeleteOrderById(id));
-    // }
+ 
+    private async Task<Customer?> GetCurrentCustomer()
+    {
+        var user = _userManager.GetUserAsync(User).Result;
+        var email = user == null? HttpContext.Session.GetString("Email") : user.Email;
+        var customerResponse = await _customerService.GetCustomerByEmail(email);
+        return customerResponse.Data;
+    }
+    
 }
